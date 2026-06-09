c86267a224d21a870e04eed1e818df575600bb19
klohith503-cyber <klohith503@gmail.com>
2026-06-09 13:55:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,52 @@
-from fastapi import FastAPI,Form
+from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
 import sqlite3
-app=FastAPI()
-conn=sqlite3.connect(
+
+app = FastAPI()
+
+conn = sqlite3.connect(
     "life.db",
     check_same_thread=False
 )
-cursor=conn.cursor()
+
+cursor = conn.cursor()
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS tasks(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-task TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task TEXT
 )
 """)
+
 conn.commit()
-@app.get("/",response_class=HTMLResponse)
+
+
+@app.get("/", response_class=HTMLResponse)
 async def home():
+
     cursor.execute(
         "SELECT task FROM tasks"
     )
-    tasks=cursor.fetchall()
-    html="""
+
+    tasks = cursor.fetchall()
+
+    html = """
+
 <!DOCTYPE html>
 <html>
+
 <head>
+
 <title>Life Command Center</title>
+
 <style>
+
 body{
 font-family:Arial;
 background:#f5f7fb;
 padding:30px;
 }
+
 .box{
 max-width:700px;
 margin:auto;
@@ -38,52 +54,70 @@ background:white;
 padding:20px;
 border-radius:20px;
 }
+
 .task{
 background:#eef2ff;
 padding:10px;
 margin-top:10px;
 border-radius:10px;
 }
+
 </style>
+
 </head>
+
 <body>
+
 <div class="box">
+
 <h1>🚀 Life Command Center</h1>
+
 <form action="/add" method="post">
+
 <input
 name="task"
 placeholder="Enter task"
-required
->
+required>
+
 <button>
 Add Task
 </button>
+
 </form>
+
 <h2>Your Tasks</h2>
+
 """
+
     for task in tasks:
-    html+=f"""
+        html += f"""
 <div class="task">
 {task[0]}
 </div>
 """
-    html+="""
+
+    html += """
 </div>
+
 </body>
 </html>
 """
+
     return html
+
+
 @app.post("/add")
-async def add(task:str=Form(...)):
+async def add(task: str = Form(...)):
+
     cursor.execute(
         "INSERT INTO tasks(task) VALUES(?)",
         (task,)
     )
+
     conn.commit()
-    return HTMLResponse(
-"""
+
+    return HTMLResponse("""
 <script>
 window.location.href="/"
 </script>
-"""
-)
+""")
